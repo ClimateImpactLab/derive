df6136f5ba83fc9243f8556c866f546f6ba3fd41
Brewster Malevich <bmalevich@rhg.com>
2020-06-18 13:24:14 -0700

    Update package setup metadata

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="glean",
     version="0.1.0a0",
-    description="",
-    url="",
+    description="Tools for processing results of the Climate Prospectus.",
+    url="https://github.com/ClimateImpactLab/glean",
     author="James Rising",
     author_email="jarising@gmail.com",
     license="MIT",
