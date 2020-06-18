f8a7c0c950549b3e036c784d68d9571f12445137
Brewster Malevich <bmalevich@rhg.com>
2020-06-18 15:59:36 -0700

    Minor code cleanup, fix bad cli arg pass

--- glean/cli/core.py
+++ glean/cli/core.py
@@ -7,15 +7,23 @@ def glean_cli():
     """Extract climate impact projection output files"""
 
 
-@glean_cli.command(help="Extract quantiles across collections of results")
-@click.argument("confpath", type=click.Path())
-def quantiles(confpath):
-    """Run the glean quantiles system with configuration file"""
+@glean_cli.command(help="Extract data from a single netCDF result file")
+@click.argument("netcdfpath", type=click.Path())
+def single(netcdfpath):
+    """Run the glean single system with configuration file"""
     raise NotImplementedError
 
 
-@glean_cli.command(help="Extract data from a single netCDF result file")
+@glean_cli.command(help="Extract quantiles across collections of results")
 @click.argument("confpath", type=click.Path())
-def single(confpath):
-    """Run the glean single system with configuration file"""
+@click.option(
+    "-c",
+    "--conf",
+    nargs=1,
+    default="",
+    multiple=True,
+    help="Additional KEY=VALUE configuration option.",
+)
+def quantiles(confpath, conf):
+    """Run the glean quantiles system with configuration file"""
     raise NotImplementedError
