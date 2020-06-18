ddc2fb67d1d0d8d24a238681e230be580401f9a2
Brewster Malevich <bmalevich@rhg.com>
2020-06-18 12:52:52 -0700

    Add simple package setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="glean",
+    version="0.1.0a0",
+    description="",
+    url="",
+    author="James Rising",
+    author_email="jarising@gmail.com",
+    license="GNU v. 3",
+    packages=find_packages(),
+    zip_safe=False,
+)
