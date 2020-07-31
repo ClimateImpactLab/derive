db1a49d63adc69f929507ec5d221ee559a224327
Brewster Malevich <bmalevich@rhg.com>
2020-07-31 16:32:43 -0700

    Swap bumpversion for setuptools_scm

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="glean",
-    version="0.1.0a0",
+    use_scm_version=True,
     description="Tools for processing results of the Climate Prospectus.",
     url="https://github.com/ClimateImpactLab/glean",
     author="James Rising",
@@ -10,6 +10,7 @@ setup(
     license="MIT",
     packages=find_packages(),
     install_requires=["click", "pyyaml", "numpy", "scipy", "statsmodels", "netCDF4"],
+    setup_requires=["setuptools_scm"],
     extras_require={
         "test": ["pytest"],
         "dev": ["pytest", "pytest-cov", "pytest-mock", "wheel", "flake8", "black", "twine"],
